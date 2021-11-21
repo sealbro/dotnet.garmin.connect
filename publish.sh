dbc0539105c0ccf7cc3b48d3cd11050a69630913
pachman <8067559+pachman@users.noreply.github.com>
2021-11-21 21:18:04 +0300

    fix CD

--- publish.sh
+++ publish.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-API_KEY=$1
+NUGET_API_KEY=$1
 
+echo "Here you see *.nupkg files:"
 cd ./output
 ls
 
 files=$(ls -t *.nupkg | head -1)
 PACKAGE_FILE="${files[0]}"
 
-echo "$PACKAGE_FILE"
+echo "Chosen [$PACKAGE_FILE] to push"
 
-if [ "$API_KEY" ]; then
-  dotnet nuget push $PACKAGE_FILE --api-key $API_KEY --source https://api.nuget.org/v3/index.json
+if [ "$NUGET_API_KEY" ]; then
+  dotnet nuget push $PACKAGE_FILE --api-key $NUGET_API_KEY --source https://api.nuget.org/v3/index.json
+  exit 0
+else
+  echo "NUGET_API_KEY was not set!"
+  exit 1
 fi
