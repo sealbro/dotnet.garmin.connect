5635daf01dd21a1406f409d5d31a2231bc5148da
Hynek <hynek@syrovatkovi.cz>
2022-02-06 12:48:15 +0100

    Another whitespace removed

--- Garmin.Connect/IGarminConnectClient.cs
+++ Garmin.Connect/IGarminConnectClient.cs
@@ -7,107 +7,107 @@ namespace Garmin.Connect;
 
 public interface IGarminConnectClient
 {
-    Task<GarminUserPreferences> GetPreferences ();
+    Task<GarminUserPreferences> GetPreferences();
 
-    Task<GarminSocialProfile> GetSocialProfile ();
+    Task<GarminSocialProfile> GetSocialProfile();
 
     /// <summary>
     /// Fetch user settings
     /// </summary>
-    Task<GarminUserSettings> GetUserSettings ();
+    Task<GarminUserSettings> GetUserSettings();
 
     /// <summary>
     /// Fetch available activity data
     /// </summary>
-    Task<GarminStats> GetUserSummary (DateTime date);
+    Task<GarminStats> GetUserSummary(DateTime date);
 
     /// <summary>
     /// Fetch available sleep data
     /// </summary>
-    Task<GarminSleepData> GetWellnessSleepData (DateTime date);
+    Task<GarminSleepData> GetWellnessSleepData(DateTime date);
 
     /// <summary>
     /// Fetch available heart rates data
     /// </summary>
-    Task<GarminHr> GetWellnessHeartRates (DateTime date);
+    Task<GarminHr> GetWellnessHeartRates(DateTime date);
 
     /// <summary>
     /// Fetch available steps data
     /// </summary>
-    Task<GarminStepsData[]> GetWellnessStepsData (DateTime date);
+    Task<GarminStepsData[]> GetWellnessStepsData(DateTime date);
 
     /// <summary>
     /// Fetch available hydration data
     /// </summary>
-    Task<GarminHydrationData> GetHydrationData (DateTime date);
+    Task<GarminHydrationData> GetHydrationData(DateTime date);
 
     /// <summary>
     /// Fetch available body composition data (only for date)
     /// </summary>
-    Task<GarminBodyComposition> GetBodyComposition (DateTime startDate, DateTime endDate);
+    Task<GarminBodyComposition> GetBodyComposition(DateTime startDate, DateTime endDate);
 
     /// <summary>
     /// Fetch personal records by owner display name
     /// </summary>
-    Task<GarminPersonalRecord[]> GetPersonalRecord (string ownerDisplayName);
+    Task<GarminPersonalRecord[]> GetPersonalRecord(string ownerDisplayName);
 
     /// <summary>
     /// Fetch device last used
     /// </summary>
-    Task<GarminDeviceLastUsed> GetDeviceLastUsed ();
+    Task<GarminDeviceLastUsed> GetDeviceLastUsed();
 
     /// <summary>
     /// Fetch device settings for current device
     /// </summary>
-    Task<GarminDeviceSettings> GetDeviceSettings (long deviceId);
+    Task<GarminDeviceSettings> GetDeviceSettings(long deviceId);
 
     /// <summary>
     /// Fetch available devices for the current account
     /// </summary>
-    Task<GarminDevice[]> GetDevices ();
+    Task<GarminDevice[]> GetDevices();
 
     /// <summary>
     /// Fetch activity split summaries
     /// </summary>
-    Task<GarminSplitSummary> GetActivitySplitSummaries (long activityId);
+    Task<GarminSplitSummary> GetActivitySplitSummaries(long activityId);
 
     /// <summary>
     /// Fetch activity details
     /// </summary>
-    Task<GarminActivityDetails> GetActivityDetails (long activityId, int maxChartSize = 2000,
+    Task<GarminActivityDetails> GetActivityDetails(long activityId, int maxChartSize = 2000,
         int maxPolylineSize = 4000);
 
     /// <summary>
     /// Fetch activity excercise sets
     /// </summary>
-    Task<GarminExcerciseSets> GetActivityExcerciseSets (long activityId);
+    Task<GarminExcerciseSets> GetActivityExcerciseSets(long activityId);
 
     /// <summary>
     /// Fetch activity weather
     /// </summary>
-    Task<GarminActivityWeather> GetActivityWeather (long activityId);
+    Task<GarminActivityWeather> GetActivityWeather(long activityId);
 
     /// <summary>
     ///  Fetch activity splits
     /// </summary>
-    Task<GarminActivitySplits> GetActivitySplits (long activityId);
+    Task<GarminActivitySplits> GetActivitySplits(long activityId);
 
     /// <summary>
     /// Fetch activity HR in timezones
     /// </summary>
-    Task<GarminHrTimeInZones[]> GetActivityHrInTimezones (long activityId);
+    Task<GarminHrTimeInZones[]> GetActivityHrInTimezones(long activityId);
 
     /// <summary>
     /// Downloads activity in requested format and returns the raw bytes. For
     /// "Original" will return the zip file content, up to user to extract it.
     /// "CSV" will return a csv of the splits.
     /// </summary>
-    Task<byte[]> DownloadActivity (long activityId, ActivityDownloadFormat format = ActivityDownloadFormat.TCX);
+    Task<byte[]> DownloadActivity(long activityId, ActivityDownloadFormat format = ActivityDownloadFormat.TCX);
 
     /// <summary>
     /// Fetch available activities
     /// </summary>
-    Task<GarminActivity[]> GetActivities (int start, int limit);
+    Task<GarminActivity[]> GetActivities(int start, int limit);
 
     /// <summary>
     /// Fetch available activities between specific dates
@@ -118,20 +118,20 @@ public interface IGarminConnectClient
     /// (Optional) Type of activity you are searching
     /// Possible values are [cycling, running, swimming, multi_sport, fitness_equipment, hiking, walking, other]
     /// </param>
-    Task<GarminActivity[]> GetActivitiesByDate (DateTime startDate, DateTime endDate, string activityType = "");
+    Task<GarminActivity[]> GetActivitiesByDate(DateTime startDate, DateTime endDate, string activityType = "");
 
     /// <summary>
     /// Fetch gear types.
     /// </summary>
-    Task<GarminGearType[]> GetGearTypes ();
+    Task<GarminGearType[]> GetGearTypes();
 
     /// <summary>
     /// Fetch user gears.
     /// </summary>
-    Task<GarminGear[]> GetUserGears (long userId);
+    Task<GarminGear[]> GetUserGears(long userId);
 
     /// <summary>
     /// Fetch activity gears.
     /// </summary>
-    Task<GarminGear[]> GetActivityGears (long activityId);
+    Task<GarminGear[]> GetActivityGears(long activityId);
 }
