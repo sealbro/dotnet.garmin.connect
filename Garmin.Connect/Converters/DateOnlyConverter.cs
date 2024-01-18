76d29b9fcebf4e32d6cf0136ddb25448a3232c4d
sealbro <8067559+sealbro@users.noreply.github.com>
2024-01-18 15:56:36 +0200

    chore: Switch to public DateTime and DateOnly converters

--- Garmin.Connect/Converters/DateOnlyConverter.cs
+++ Garmin.Connect/Converters/DateOnlyConverter.cs
@@ -3,7 +3,8 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 
 namespace Garmin.Connect.Converters;
-internal class DateOnlyConverter : JsonConverter<DateOnly>
+
+public class DateOnlyConverter : JsonConverter<DateOnly>
 {
     public override DateOnly Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
     {
