af36d0716356fc2f6e6c0ec2f97568fc597892d2
sealbro <8067559+sealbro@users.noreply.github.com>
2024-12-08 10:35:21 +0200

    fix(api): static user-agent #94

--- Garmin.Connect/Auth/StaticUserAgent.cs
+++ Garmin.Connect/Auth/StaticUserAgent.cs
@@ -2,5 +2,16 @@ namespace Garmin.Connect.Auth;
 
 public class StaticUserAgent : IUserAgent
 {
-    public string New => "Mozilla/5.0 (iPhone; CPU iPhone OS 16_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148";
+    private const string DefaultUseAgent = "GCM-iOS-5.7.2.1";
+
+    public string New { get; }
+
+    public StaticUserAgent() : this(DefaultUseAgent)
+    {
+    }
+
+    private StaticUserAgent(string defaultUseAgent)
+    {
+        New = defaultUseAgent;
+    }
 }
