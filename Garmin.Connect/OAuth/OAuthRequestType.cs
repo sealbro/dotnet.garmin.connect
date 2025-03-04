a873efdd66d44288eebfafe0d5136d7ec2d53c3e
sealbro <8067559+sealbro@users.noreply.github.com>
2025-03-04 16:06:31 +0200

    chore(oauth): Copy deprecated OAuth deps

---
+++ Garmin.Connect/OAuth/OAuthRequestType.cs
@@ -0,0 +1,14 @@
+﻿namespace OAuth
+{
+    /// <summary>
+    /// The types of OAuth requests possible in a typical workflow.
+    /// Used for validation purposes and to build static helpers.
+    /// </summary>
+    internal enum OAuthRequestType
+    {
+        RequestToken,
+        AccessToken,
+        ProtectedResource,
+        ClientAuthentication
+    }
+}
---
+++ Garmin.Connect/OAuth/WebUtils.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Web;
+
+namespace OAuth
+{
+    internal static class WebUtils
+    {
+        public static IEnumerable<WebParameter> ParseQueryString(Uri uri)
+        {
+            if (uri is null) { throw new ArgumentNullException(nameof(uri)); }
+
+            NameValueCollection parsedQuery = HttpUtility.ParseQueryString(uri.Query);
+            var queryStringParameters =
+                parsedQuery.AllKeys.SelectMany(parsedQuery.GetValues, (key, value) => new {key, value});
+
+            // ReSharper disable once PossibleMultipleEnumeration
+            foreach (var param in queryStringParameters)
+            {
+                yield return new WebParameter(param.key, param.value);
+            }
+        }
+    }
+}
